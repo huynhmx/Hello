c8737951363346728e55cf0e8856b9753adb4e60
huynhmx <89534030+huynhmx@users.noreply.github.com>
2021-08-25 11:16:14 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/************************************************************
+ * Java Program Name
+*************************************************************
+*Student Name Michael Huynh
+*Date Created 8-25-2021
+*Course Number and Section Number
+************************************************************/
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Your Name>");
+		System.out.println("<Java Program Name>");
+		System.out.println("<Program/Prject Description");
+		System.out.println("<Date Created>");
+		System.out.println("<Course Number and Section Number>");
+		System.out.println();
+	}
+}
